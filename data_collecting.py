3c520c691cf5f7dd3ef8f8d61022e6c67a196356
psyconorlogy <faircloughconor@gmail.com>
2023-07-01 18:53:30 +0100

    Fixed API issue

--- data_collecting.py
+++ data_collecting.py
@@ -19,6 +19,6 @@ def get_stock_data(stock_name, start_date, end_date):
         start = datetime.strptime(start_date, '%Y-%m-%d'),
         end = datetime.strptime(end_date,'%Y-%m-%d') 
     )
-    data=client.get_crypto_bars(request)
+    data=client.get_crypto_bars(request, feed="us")
     data.df
     return data
--- get_stock_test.py
+++ get_stock_test.py
@@ -1,2 +1,3 @@
 import data_collecting as data
-data.get_stock_data("BTC/USD", "2022-07-01", "2022-07-05")
+print(data.get_stock_data("BTC/USD", "2022-07-01", "2022-07-05"))
+
